bce5e97d68f9ef2b180752cb9e68032f2b8318dd
lennonsousa <lennonsousa@outlook.com>
2021-03-29 20:48:50 -0300

    Express

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const path = require("path");
+import express from 'express';
+import path from 'path';
+const app = express();
 
 const configs = {
     caminho: "build", //Aqui será definido a pasta de saída onde contém o index.html e os outros arquivos.
@@ -6,23 +8,20 @@ const configs = {
     port: process.env.PORT || 3000
 }
 
-const express = require("express");
-const server = express();
-
 if (configs.forcarHTTPS) //Se o redirecionamento HTTP estiver habilitado, registra o middleware abaixo
-    server.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
+    app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
         if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
             res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS
         else //Se a requisição já é HTTPS
             next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
     });
 
-server.use(express.static(configs.caminho)); //Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
+app.use(express.static(configs.caminho)); //Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
 
-server.get("*", (req, res) => {// O wildcard '*' serve para servir o mesmo index.html independente do caminho especificado pelo navegador.
+app.get("*", (req, res) => {// O wildcard '*' serve para servir o mesmo index.html independente do caminho especificado pelo navegador.
     res.sendFile(path.join(__dirname, configs.caminho, "index.html"));
 });
 
-server.listen(configs.port, () => {
+app.listen(configs.port, () => {
     console.log(`Escutando na ${configs.port}!`);
 });
